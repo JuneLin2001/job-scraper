0aee350470e46964ff0972c4eaf40ebb734b964c
Yen-Chun Lin <asd900606@gmail.com>
2025-10-21 18:21:02 +0800

    feat(frontend): fetch fastAPI data

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,51 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { ref, onMounted } from "vue";
+  import Card from "./components/Card.vue";
+
+  interface Job {
+    id: number;
+    title: string;
+    salary: string;
+    company_name: string;
+    location: string;
+    link?: string;
+    description?: string;
+  }
+
+  const jobs = ref<Job[]>([]);
+  const loading = ref(true);
+  const error = ref<string | null>(null);
+
+  const fetchJobs = async () => {
+    try {
+      const res = await fetch("http://localhost:8000/api/jobs/");
+      if (!res.ok) throw new Error("Failed to fetch jobs");
+      const data = await res.json();
+      jobs.value = data.jobs;
+    } catch (err: any) {
+      error.value = err.message;
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  onMounted(() => {
+    fetchJobs();
+  });
+</script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  <div class="card w-96 bg-base-100 card-xs shadow-sm">
-    <div class="card-body">
-      <h2 class="card-title">Xsmall Card</h2>
-      <p>
-        A card component has a figure, a body part, and inside body there are
-        title and actions parts
-      </p>
-      <div class="justify-end card-actions">
-        <button class="btn btn-primary">Buy Now</button>
-      </div>
+  <h1 class="text-3xl font-bold underline mb-4">Job List</h1>
+  <div v-if="loading">Loading...</div>
+  <div v-if="error" class="text-red-500">{{ error }}</div>
+
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div
+      v-for="job in jobs"
+      :key="job.id"
+      class="card w-full bg-base-100 shadow-md"
+    >
+      <Card :job="job" />
     </div>
   </div>
 </template>
