db10a35878e01d93a7f8f6ad2becb63b865cfd59
Yen-Chun Lin <asd900606@gmail.com>
2025-10-21 18:20:40 +0800

    fix: CORS issue

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,21 @@ from fastapi import FastAPI
 from routers import jobs, scrape
 import models
 from database import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -13,5 +26,5 @@ async def root():
 models.Base.metadata.create_all(bind=engine)
 
 
-app.include_router(jobs.router)
-app.include_router(scrape.router)
+app.include_router(jobs.router, prefix="/api")
+app.include_router(scrape.router, prefix="/api")
