55b58fccb97c7e05e985aa550d31eff562fcf7bc
Yen-Chun Lin <asd900606@gmail.com>
2025-10-21 18:02:50 +0800

    feat(frontend): add DaisyUI

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,4 +2,16 @@
 
 <template>
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div class="card w-96 bg-base-100 card-xs shadow-sm">
+    <div class="card-body">
+      <h2 class="card-title">Xsmall Card</h2>
+      <p>
+        A card component has a figure, a body part, and inside body there are
+        title and actions parts
+      </p>
+      <div class="justify-end card-actions">
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
+    </div>
+  </div>
 </template>
