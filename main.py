043854900848c18e98d81341c759ac3468720901
Yen-Chun Lin <asd900606@gmail.com>
2025-10-21 16:13:32 +0800

    feat: add model and database setting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from routers import source
+import models
+from database import engine
 
 app = FastAPI()
 
@@ -7,5 +9,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+models.Base.metadata.create_all(bind=engine)
+
 
 app.include_router(source.router)
